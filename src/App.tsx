b029df6d4de776d5b3cf2357925e46fe429ba8b4
xiayumu <xiayumu@zjhongsen.cn>
2021-09-01 10:01:25 +0800

    提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,98 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Spin } from 'antd';
 import './App.css';
 import { ReactComponent as ReactLog } from './style/svg/a.svg'
 import './style/logo.scss'
 import Login from './views/login/login'
 import Test from './views/test_index/test'
+import DynamicAntdTheme from 'dynamic-antd-theme';
+import 'antd/dist/antd.dark.less'
+import { useState } from 'react'
 
-
+// function addSkin(params: string) {
+//   let head = document.getElementsByTagName('head')[0]
+//   const getLink = head.getElementsByTagName('link')
+//   // 查找style是否存在，存在的话就删除dom
+//   if (getLink.length > 0) {
+//     for (let i = 0; i < getLink.length; i++) {
+//       if (getLink[i].getAttribute('data-type') === 'theme') {
+//         getLink[i].remove()
+//       }
+//     }
+//   }
+//   // 查找script是否存在
+//   const getScript = head.getElementsByTagName('script')
+//   if (getScript.length > 0) {
+//     for (let i = 0; i < getScript.length; i++) {
+//       if (getScript[i].getAttribute('data-type') === 'theme') {
+//         getScript[i].remove()
+//       }
+//     }
+//   }
+//   let link = document.createElement('link')
+//   link.dataset.type = 'theme'
+//   link.href = params
+//   link.rel = 'stylesheet'
+//   link.type = 'text/css'
+//   head.appendChild(link)
+//   let script = document.createElement('script')
+//   script.type = 'text/javascript'
+//   script.dataset.type = 'theme'
+//   script.src = 'https://cdn.bootcdn.net/ajax/libs/less.js/4.1.1/less.js'
+//   head.appendChild(script) 
+//   // 最后加入对应的主题和加载的css文件
+//   // let styleDom = document.createElement('style')
+//   // styleDom.dataset.type = 'theme'
+//   // styleDom.innerHTML = params
+//   // head.appendChild(styleDom)
+// }
 function App() {
+  const [count, setCount] = useState(false)
+  const [lighter, setLighter] = useState('')
+  const [dark, setDark] = useState('')
+  const [state, setState] = useState(false)
+  const switchs = () => {
+    setCount(!count)
+    setState(true)
+    let head = document.getElementsByTagName('head')[0]
+    let getLink = head.getElementsByTagName('style')
+    if (count) {
+      
+      if (!lighter.length) {
+        // setState(false)
+        
+        // console.log('aaaaaaaaaaaa',less);
+        
+        setLighter(getLink[getLink.length - 1].innerHTML)
+      } else {
+        getLink[getLink.length - 1].innerHTML = lighter
+        console.log(111);
+        // setState(false)
+      }
+    } else {
+      if (!dark.length) {
+        // setState(false)
+        require('antd/dist/antd.dark.css.map')
+        setDark(getLink[getLink.length - 1].innerHTML)
+      } else {
+        getLink[getLink.length - 1].innerHTML = dark
+        console.log(222);
+        // setState(false)
+      }
+    }
+    console.log('getLink', getLink);
+    
+  }
   return (
     <div className="App">
       <Login />
       <Test />
       <ReactLog className='App-logo'/>
-      <Button type="primary">Button</Button>
+      <Button type="primary" onClick={() => switchs() }>Button</Button>
+      <div className='theme-container'>
+        <span>Change antd theme:{count} </span>
+        <DynamicAntdTheme />
+      </div>
     </div>
   );
 }
